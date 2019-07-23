debba568ffa72fc81126b467b0d55c8a5cd83b1f
Andreas Lillebø <git@andreaslillebo.com>
2019-07-23 15:41:34 +0900

    Add ByeBug dev dependency

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,8 @@ require 'stripe'
 require 'stripe_mock'
 require 'stripe_mock/server'
 
+require 'byebug'
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir["./spec/support/**/*.rb"].each {|f| require f}
